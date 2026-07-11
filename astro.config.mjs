4bbca2933a975653d46fb2d2dc6a2c4b1badc31e
Meinom <137494458+Meinom@users.noreply.github.com>
2026-07-11 16:10:04 +0200

    Update astro config and trigger redeployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Enable strict mode for better error checking
+  site: 'https://meinom.github.io',
   vite: {
     ssr: {
       external: ["svgo"]
